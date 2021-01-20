48e509c3d8756543ace71328abc32315114fcb7d
Phillip Pearson <pp@myelin.nz>
2021-01-19 20:31:39 -0800

    Add support for Myelin Arcflash and POST Box boards

---
+++ boards/myelin_postbox/board_config.h
@@ -0,0 +1,20 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Myelin"
+#define PRODUCT_NAME "POST Box"
+#define VOLUME_LABEL "POSTBOOT"
+#define INDEX_URL "http://myelin.nz/acorn/postbox"
+#define BOARD_ID "SAMD21E18A-POSTBox-r3"
+
+#define USB_VID 0x1209
+#define USB_PID 0xFE06
+
+#define LED_PIN PIN_PA22
+
+#define BOARD_NEOPIXEL_PIN PIN_PA19
+#define BOARD_NEOPIXEL_COUNT 1
+
+#endif
