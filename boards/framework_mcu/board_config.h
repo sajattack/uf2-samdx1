a9f1b44c59295a6fe7e755cf976e390bf83be5c5
Paul Sajna <hello@paulsajna.com>
2024-11-12 02:27:03 -0800

    Add Framework Microcontroller Expansion Card board definition

---
+++ boards/framework_mcu/board_config.h
@@ -0,0 +1,18 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Paul Sajna"
+#define PRODUCT_NAME "Framework Microcontroller Expansion Card"
+#define VOLUME_LABEL "FWMCUBOOT"
+#define INDEX_URL "https://github.com/sajattack/framework_mcu_expansion_card"
+#define BOARD_ID "SAMD21G18A-Framework-MCU-v0"
+
+#define USB_VID 0x1209
+#define USB_PID 0xDADD
+
+#define BOARD_RGBLED_CLOCK_PIN            PIN_PA19
+#define BOARD_RGBLED_DATA_PIN             PIN_PA18
+
+#endif
